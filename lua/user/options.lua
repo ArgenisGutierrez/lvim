fb21d196404addba7f718f30453f4e7226568bce
ArgenisGutierrez <argenis.v.gtz@gmail.com>
2024-01-13 17:06:30 -0600

    solucion de error ntree
    se soluciono el error al cargar algunas carpetas y tipos de archivos del ntree

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -5,3 +5,38 @@ vim.opt.shell = "/bin/zsh"
 vim.o.wrap = true
 vim.o.relativenumber = true
 vim.o.encoding = 'utf-8'
+
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "javascript",
+  "json",
+  "lua",
+  "typescript",
+  "css",
+  "yaml",
+  "pug",
+  "dockerfile",
+}
+
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
+lvim.builtin.nvimtree.setup.filters.custom = {}
+
+lvim.builtin.treesitter.ignore_install = {}
+lvim.builtin.treesitter.highlight.enabled = true
+
+lvim.builtin.project.detection_methods = { "lsp", "pattern" }
+lvim.builtin.project.patterns = {
+  ".git",
+  "package-lock.json",
+  "yarn.lock",
+  "package.json",
+  "requirements.txt",
+}
+
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+lvim.builtin.telescope.defaults.path_display = {
+  shorten = 4,
+}
